e0068c1803a5ae50be773aab7de3e0bd15dd9ec4
Corey Stewart <decadentsavant@gmail.com>
2022-08-27 09:36:56 -0400

    Added data_error class.

--- lib/model/data_error.dart
+++ lib/model/data_error.dart
@@ -1,9 +1,9 @@
 import 'package:equatable/equatable.dart';
 
 class DataError extends Equatable {
-  final String message;
 
   const DataError({this.message = ''});
+  final String message;
 
   @override
   List<Object?> get props => [message];
